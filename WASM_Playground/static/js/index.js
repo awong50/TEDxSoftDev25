f335ef3c47bc6863d5cf95becfd987aef1921c81
Aidan <aidanwonglam@gmail.com>
2025-02-09 19:13:21 -0500

    Added Python in browser runner using Pyodide

---
+++ WASM_Playground/static/js/index.js
@@ -0,0 +1,28 @@
+async function main() {
+    let pyodide = await loadPyodide();
+    console.log("Pyodide loaded.");
+
+    document.getElementById("pythonForm").addEventListener("submit", async function(event) {
+        event.preventDefault();
+
+        let code = document.getElementById("codeInput").value;
+        console.log(code)
+        let outputElement = document.getElementById("output");
+
+        try {
+            pyodide.runPython(`
+                import io
+                import sys
+                sys.stdout = io.StringIO()
+              `);
+            let result = pyodide.runPython(code);
+            let stdout = pyodide.runPython("sys.stdout.getvalue()");
+            console.log(stdout)
+            outputElement.textContent = stdout;
+        } catch (error) {
+            outputElement.textContent = "Error: " + error.message;
+        }
+    });
+}
+
+main();
